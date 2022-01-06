4bc6941ea5d3164212684b12d647f5e27fe7be71
Truongdangkhoa2008110021 <92006636+Truongdangkhoa2008110021@users.noreply.github.com>
2022-01-06 09:39:12 +0700

    Create Hangdienmay.java

---
+++ Hangdienmay.java
@@ -0,0 +1,32 @@
+/*
+*Ngày tạo : 29 thg 12, 2021
+*Tác giả : Trương Đăng Khao
+*/
+package com.Khoatd2008110021.BaiKiemTraCuoiKy;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+public class HangDienMay extends HangHoa {
+    private int congSuat;
+    public HangDienMay(String maHang,String tenHangHoa, long giaNhap, int soLuongTonKho, String ngayNhapKho , int congSuat) throws ParseException {
+        super("HDM-" + maHang,tenHangHoa, giaNhap, soLuongTonKho, ngayNhapKho);
+        this.congSuat = congSuat;
+        //TODO Auto-generated constructor stub
+    }
+    public void setCongSuat(int congSuat) {
+        this.congSuat = congSuat;
+    }
+    public int getCongSuat() {
+        return congSuat;
+    }
+    DecimalFormat ft = new DecimalFormat("##### W");
+    DecimalFormat ft1 = new DecimalFormat("###,###,### VND");
+    @Override
+    public String toString() {
+        String s ;
+        s = super.toString() + String.format("%-20s|%-20s|", ft.format(getCongSuat()),ft1.format(tinhTien()));
+        return s ;
+    }
+    
+}
